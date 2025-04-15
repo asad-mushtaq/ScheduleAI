6f64856e7fd524c95f56836fbf6580d571b62038
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2025-04-15 13:44:15 -0700

    removed test test

--- db_manager/src/tests/services/verification.test.ts
+++ db_manager/src/tests/services/verification.test.ts
@@ -1,10 +1,5 @@
 import BadRequestError from "../../models/errors/bad-request-error.js";
-import { stringToInteger } from "../../services/converters.js";
 import { verifyId } from "../../services/verification.js";
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(stringToInteger("3")).toBe(3);
-});
 
 test('3.3 is not a valid ID', () => {
     try {
